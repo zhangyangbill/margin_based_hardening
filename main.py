dd6abf5d8549112556642005c9f206d634058658
Yang Zhang <zhangyangbill@gmail.com>
2018-01-18 12:29:09 -0500

    modify main

--- main.py
+++ main.py
@@ -19,10 +19,10 @@ from tensorflow.examples.tutorials.mnist import input_data
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 
 # run compute margin
-for i in xrange(2, 100):
+for i in xrange(100):
     batch = mnist.train.next_batch(1)
     dist, closest_point, pred_class = m.compute_margin(batch[0], batch[1], [1],
-                                                       num_iterations = 10)
+                                                       num_iterations = 200)
     
     # save data
     if not os.path.exists('/mnt/nfs/nfsshare/user_homes/zybill/results/{}/'.format(i)):
